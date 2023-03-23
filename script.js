0fe78bc2fee6b7818f91c7fe6e84844b3c1f5d56
Piotr Ochodek <ohis@op.pl>
2023-03-23 09:46:42 +0100

    added to function rule spread operator

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-const arr1 = [1,2]
-const arr2 = [3,4]
-const arr3 = [5,6]
+const arr1 = [1, 2];
+const arr2 = [3, 4];
+const arr3 = [5, 6];
 
 const rule = (...arr) => {
-    return [].concat(...arr)
-}
+  return [...arr].flat();
+};
 
-const oneArray = rule(arr1, arr2)
-const oneArray2 = rule(arr1, arr2, arr3)
-console.log(oneArray)
-console.log(oneArray2)
+const oneArray = rule(arr1, arr2);
+const oneArray2 = rule(arr1, arr2, arr3);
+const allArrays = oneArray.concat(oneArray2);
+const allArrays2 = [...oneArray, ...oneArray2];
+console.log(allArrays);
+console.log(allArrays2);
+console.log(oneArray);
+console.log(oneArray2);
